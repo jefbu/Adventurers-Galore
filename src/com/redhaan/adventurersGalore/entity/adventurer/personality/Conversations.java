a24b13a9ecfcfa4eb31419f75279f66796bb56f2
jefbu <jefbu@LAPTOP-2RJ5EJNK>
2020-06-29 22:13:04 +0200

    quick fix

--- src/com/redhaan/adventurersGalore/entity/adventurer/personality/Conversations.java
+++ src/com/redhaan/adventurersGalore/entity/adventurer/personality/Conversations.java
@@ -46,13 +46,12 @@ public class Conversations extends GameObject {
 
 		case WorldMap:
 
-//<<<<<<< HEAD
+
 				if(random.nextInt(6000000) == 0) { 
-//=======
+
 			if (!active) {
 
 				if (random.nextInt(60) == 0) {
-//>>>>>>> branch 'master' of https://github.com/jefbu/Adventurers-Galore.git
 					ArrayList<Adventurer> candidates = new ArrayList<Adventurer>();
 					for (Adventurer adventurer : GameManager.adventurers.allAdventurers) {
 						if (adventurer.inParty) {
