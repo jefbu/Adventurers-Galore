8368d17cc2ad035fb5f529436a178217f4a31ef0
jefbu <jefbussens@gmail.com>
2020-11-29 08:08:38 +0100

    v0.651 alignment of portrait UI

--- src/com/redhaan/adventurersGalore/quest/questUI/Portrait.java
+++ src/com/redhaan/adventurersGalore/quest/questUI/Portrait.java
@@ -33,8 +33,8 @@ public class Portrait extends GameObject {
 	@Override
 	public void render(GameContainer gameContainer, Renderer renderer) {
 				
-		try { renderer.drawImageTile(icon, 60, 210, QuestUI.quest.requester.genderInt, 0); } catch (Exception e) { }
-		try { renderer.drawText(name, 70, 260, 0xff996644); } catch (Exception e) { }
+		try { renderer.drawImageTile(icon, 97, 225, QuestUI.quest.requester.genderInt, 0); } catch (Exception e) { }
+		try { renderer.drawText(name, 95, 270, 0xff996644); } catch (Exception e) { }
 
 		
 	}
