cb5d5d6e4ad583ea038694e4543b36877d193b82
jefbu <jefbussens@gmail.com>
2021-08-22 19:35:34 +0200

    v0.822 fixing git

--- src/com/redhaan/adventurersGalore/entity/adventurer/personality/ConversationTable.java
+++ src/com/redhaan/adventurersGalore/entity/adventurer/personality/ConversationTable.java
@@ -100,7 +100,6 @@ public abstract class ConversationTable {
 			conversation.lines.add(new Line("What an interesting story. And it sure needed to be this long.", 1));
 			conversation.talkers = talkers;
 			return conversation;
-			//comment
 			
 			
 		case 99:
